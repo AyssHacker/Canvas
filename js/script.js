3d9a0a1b6f303cb001df31f7868fe9d6720b48a5
AyssHacker <hackerayss@gmail.com>
2021-09-11 17:41:16 +0800

    test

--- js/script.js
+++ js/script.js
@@ -1,48 +1,35 @@
-let canvas = document.getElementById('canvas')
-let ctx = canvas.getContext("2d")
+let canvas = document.getElementById("canvas");
 canvas.width = document.documentElement.clientWidth
 canvas.height = document.documentElement.clientHeight
-
-ctx.fillStyle = "black"
-ctx.strokeStyle = "none"
-ctx.clientWidth = 8
-ctx.lineCap = 'round'
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.strokeStyle = 'none'
+ctx.lineWidth = 20;
+ctx.lineCap = "round";
 
 function drawLine(x1, y1, x2, y2) {
-    ctx.beginPath()
-    ctx.moveTo(x1, y1)
-    ctx.lineTo(x2, y2)
-    ctx.stroke()
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
 }
-
-
 let painting = false
-let last = []
-let isTouchDevice = 'ontouchstart' in document.documentElement
+let last
+var isTouchDevice = 'ontouchstart' in
+    document.documentElement;
 
 if (isTouchDevice) {
-    canvas.ontouchStart = (e) => {
-        //  painting = true
+    canvas.ontouchstart = (e) => {
         let x = e.touches[0].clientX
         let y = e.touches[0].clientY
         last = [x, y]
-
-
     }
     canvas.ontouchmove = (e) => {
-            let x = e.touches[0].clientX
-            let y = e.touches[0].clientY
-                //  if (painting === true) {
-            drawLine(last[0], last[1], x, y)
-            last = [x, y]
-                // }
-
-
-        }
-        // canvas.ontouchend = e => {
-        //     painting = false
-
-    // }
+        let x = e.touches[0].clientX
+        let y = e.touches[0].clientY
+        drawLine(last[0], last[1], x, y)
+        last = [x, y]
+    }
 } else {
     canvas.onmousedown = (e) => {
         painting = true
@@ -52,8 +39,9 @@ if (isTouchDevice) {
         if (painting === true) {
             drawLine(last[0], last[1], e.clientX, e.clientY)
             last = [e.clientX, e.clientY]
-        }
+        } else {
 
+        }
     }
     canvas.onmouseup = () => {
         painting = false
